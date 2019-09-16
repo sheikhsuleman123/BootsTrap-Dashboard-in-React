0d32cabd3d5849843958a5f7ff2571911c99cc82
sheikhsuleman123 <mssheikh333@gmail.com>
2019-09-16 14:18:46 +0500

    pagination is ok

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -224,6 +224,34 @@ export default class Navbar extends Component {
                   </tr>
                 </tbody>
                  </table>
+                 {/* Pagination is started from here  */}
+                 <nav>
+                   <ul className="pagination justify-content-center">
+                      <li className="page-item">
+                        <a href="#" className="page-link py-2 px-3">
+                          <span>&laquo; </span>
+                        </a>
+                      </li>
+                      <li className="page-item active">
+                        <a href="#" className="page-link py-2 px-3">
+                          1
+                        </a>
+                      </li><li className="page-item">
+                        <a href="#" className="page-link py-2 px-3">
+                          2
+                        </a>
+                      </li><li className="page-item">
+                        <a href="#" className="page-link py-2 px-3">
+                          3
+                        </a>
+                      </li>
+                      <li className="page-item">
+                        <a href="#" className="page-link py-2 px-3">
+                          <span>&raquo; </span>
+                        </a>
+                      </li>
+                   </ul>
+                 </nav>
               </div>
               <div className="col-12">
                 
