40196a4d30bec263dff22f77bff4061dddbdca66
sheikhsuleman123 <mssheikh333@gmail.com>
2019-09-18 13:14:48 +0500

    date and time section is ok

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -535,8 +535,47 @@ export default class Navbar extends Component {
                     </div>
                   </div>
                 </div>
-                <div className="col-xl-5">
-
+                <div className="col-xl-5 mt-5">
+                  <div className="card rounded">
+                    <div className="card-body">
+                      <h5 className="text-muted text-center mb-4">Quick Status Post</h5>
+                    <ul className="list-inline text-center py-3">
+                      <li className="list-inline-item mr-4">
+                        <a href=""><i className="fas fa-pencil-alt text-success"> </i><span className="h6 text-muted">Status</span> 
+                        </a> </li> 
+                        <li className="list-inline-item mr-4">
+                        <a href=""><i className="fas fa-camera text-info"> </i><span className="h6 text-muted">Photo</span> 
+                        </a> </li>
+                        <li className="list-inline-item mr-4">
+                        <a href=""><i className="fas fa-map-marker-alt text-primary"> </i><span className="h6 text-muted">Check in</span> 
+                        </a> </li>
+                      </ul>
+                      <form >
+                        <div className="form-group">
+                          <input type="text" className="form-control py-2 mb-3" placeholder="whats your status..?" />
+                          <button type="button" className="btn btn-block text-uppercase font-weight-bold text-light bg-info py-2 mb-5">Submit</button>
+                        </div>
+                      </form>
+                      <div className="row">
+                        <div className="col-6">
+                          <div className="card bg-light">
+                          <i className="far fa-calendar-alt fa-8x text-warning d-block m-auto py-3"></i>
+                          <div className="card-body">
+                            <p className="card-text text-center font-weight-bold text-uppercase">Mon, may 20</p>
+                          </div>
+                          </div>
+                        </div>
+                        <div className="col-6">
+                        <div className="card bg-light">
+                          <i className="far fa-clock fa-8x text-danger d-block m-auto py-3"></i>
+                          <div className="card-body">
+                            <p className="card-text text-center font-weight-bold text-uppercase">2:00 am</p>
+                          </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
