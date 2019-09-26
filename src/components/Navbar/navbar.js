d96df96ea479d59e8d845e2d690aeb82477d196b
sheikhsuleman123 <mssheikh333@gmail.com>
2019-09-26 13:17:07 +0500

    footer section is done

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -582,6 +582,34 @@ export default class Navbar extends Component {
           </div>
         </div>
       </section>
+      {/* Footer section is going started from here  */}
+      <footer>
+        <div className="container-fluid">
+          <div className="col-xl-10 col-lg-9 col-md-8 ml-auto">
+            <div className="row border-top pt-3 mt-3">
+              <div className="col-lg-6 text-center">
+                <ul className="list-inline">
+                  <li className="list-inline-item mr-2">
+                    <a href="" className="">CodeAndCreate</a>
+                  </li>
+                  <li className="list-inline-item mr-2">
+                    <a href="" className="">About</a>
+                  </li>
+                  <li className="list-inline-item mr-2">
+                    <a href="" className="">Support</a>
+                  </li>
+                  <li className="list-inline-item mr-2">
+                    <a href="" className="">Blog</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-lg-6 text-center">
+                <p>&copy; 2019 Copyright Made with <i className="fas fa-heart text-danger"></i> by <span className="text-success">CodeAndCreate</span> </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
     );
   }
